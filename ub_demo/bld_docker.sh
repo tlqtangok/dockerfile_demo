ad718669f98ec73cefe8c45319d36d2008f7c0c1
Jidor Tang <tlqtangok@126.com>
2019-05-28 16:51:09 +0800

    - edit bld_docker.sh at 20190528_1651

--- ub_demo/bld_docker.sh
+++ ub_demo/bld_docker.sh
@@ -1,2 +1,8 @@
-docker build -t ub_sshd:v11 .
+export VERSION=V13
+docker build -t ub_sshd:$VERSION .
 #docker run -d -p 2222:2222 --name id_b_jd_v0 id_b:jd_v0
+
+# docker run  -dit -p 10200:22 --name 'c0' -h 'jd' ub_sshd:$VERSION
+# ssh root@localhost -p 10206
+
+
