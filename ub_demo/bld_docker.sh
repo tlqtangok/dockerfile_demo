7c3d00fdc6b9f9564033fe693c859ffc1734fb64
Jidor Tang <tlqtangok@126.com>
2019-05-28 20:57:34 +0800

    add entrypoint, which can run script.sh

--- ub_demo/bld_docker.sh
+++ ub_demo/bld_docker.sh
@@ -1,8 +1,13 @@
-export VERSION=V13
+export VERSION=v20
 docker build -t ub_sshd:$VERSION .
+
 #docker run -d -p 2222:2222 --name id_b_jd_v0 id_b:jd_v0
 
-# docker run  -dit -p 10200:22 --name 'c0' -h 'jd' ub_sshd:$VERSION
+# docker run  -dit -p 10200:22 --name $VERSION -h $VERSION ub_sshd:$VERSION
 # ssh root@localhost -p 10206
+
+# lsi
+# lsc
+# docker rm -f `lsc | grep -v CON|ff 0|tol`
 
 
--- ub_demo/start.sh
+++ ub_demo/start.sh
@@ -1,1 +1,6 @@
-service ssh start
+#!/bin/sh
+
+#service ssh start
+date 
+echo "- successfully start this container"
+/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
