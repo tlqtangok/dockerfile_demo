9e9db046511480d03407e5ed14a89b1a3ed27e63
Jidor Tang <tlqtangok@126.com>
2019-05-29 17:26:54 +0800

    - edit bld_docker.sh at 20190529_1726

--- ub_cuda_demo/bld_docker.sh
+++ ub_cuda_demo/bld_docker.sh
@@ -4,7 +4,7 @@ docker build -t ub_cuda_sshd:$VERSION .
 #docker run -d -p 2222:2222 --name id_b_jd_v0 id_b:jd_v0
 
 # docker run  -dit -p 10200:22 --name $VERSION -h $VERSION ub_sshd:$VERSION
-# docker run  --runtime=nvidia -it -p 10200:22 --name $VERSION -h $VERSION ub_sshd:$VERSION
+# docker run  --runtime=nvidia -it -p 10200:22 --name $VERSION -h $VERSION ub_cuda_sshd:$VERSION
 # ssh root@localhost -p 10206
 
 # lsi
