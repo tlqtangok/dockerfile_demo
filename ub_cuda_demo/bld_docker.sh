7aec3f00bfdae496d94389716dafe2866f727c1c
Jidor Tang <tlqtangok@126.com>
2019-05-30 09:35:09 +0800

    add start many container

--- ub_cuda_demo/bld_docker.sh
+++ ub_cuda_demo/bld_docker.sh
@@ -5,11 +5,15 @@ docker build -t ub_cuda_sshd:$VERSION .
 
 # docker run  -dit -p 10200:22 --name $VERSION -h $VERSION ub_sshd:$VERSION
 # docker run  --runtime=nvidia -it -p 10200:22 --name $VERSION -h $VERSION ub_cuda_sshd:$VERSION
-# docker run  --runtime=nvidia -it -p 10200:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name $VERSION -h $VERSION ub_cuda_sshd:$VERSION
 # ssh root@localhost -p 10200
 
 # lsi
 # lsc
 # docker rm -f `lsc | grep -v CON|ff 0|tol`
 
+# need change the ngrok, with its configure file
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10203:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name $VERSION     -h $VERSION     ub_cuda_sshd:$VERSION
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10200:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_0 -h ${VERSION}_0 ub_cuda_sshd:$VERSION 
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10201:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_1 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10202:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_2 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
 
