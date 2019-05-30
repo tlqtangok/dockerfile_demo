b8d4416599bcb4f3e2bb931543435d0678bc70d7
Jidor Tang <tlqtangok@126.com>
2019-05-30 14:54:52 +0800

    add ub demo, with cmd last line /bin/bash

--- ub_demo/bld_docker.sh
+++ ub_demo/bld_docker.sh
@@ -1,13 +1,16 @@
-export VERSION=v23
+export VERSION=v2
 docker build -t ub_sshd:$VERSION .
 
-#docker run -d -p 2222:2222 --name id_b_jd_v0 id_b:jd_v0
-
-# docker run  -dit -p 10200:22 --name $VERSION -h $VERSION ub_sshd:$VERSION
-# ssh root@localhost -p 10206
+# export VERSION=v2 && docker run  -dit -p 10204:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t -m 100m --cpuset-cpus="1,2" --name ${VERSION}_4 -h ${VERSION}_4 ub_sshd:$VERSION
+# ssh root@localhost -p 10200
 
 # lsi
 # lsc
 # docker rm -f `lsc | grep -v CON|ff 0|tol`
 
+# need change the ngrok, with its configure file
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10203:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name $VERSION     -h $VERSION     ub_cuda_sshd:$VERSION
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10200:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_0 -h ${VERSION}_0 ub_cuda_sshd:$VERSION 
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10201:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_1 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10202:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_2 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
 
--- ub_demo/start.sh
+++ ub_demo/start.sh
@@ -2,5 +2,10 @@
 
 #service ssh start
 date 
+# export JD=JD_ENV_VAR_VALUE  # no meanings 
 echo "- successfully start this container"
-/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
+#bash -c "bash  /root/jd/t/ngrok/start_ngrok.sh &" &
+#/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
+
+service ssh start &
+/bin/bash
