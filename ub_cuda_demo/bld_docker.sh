cbff23c2beb285f9a90284f16f3c98d7bad7d8d8
Jidor Tang <tlqtangok@126.com>
2019-05-31 16:49:04 +0800

    - edit bld_docker.sh at 20190531_1649

--- ub_cuda_demo/bld_docker.sh
+++ ub_cuda_demo/bld_docker.sh
@@ -1,10 +1,8 @@
 export VERSION=v21
+# docker build -t ub_sshd:$VERSION .
 docker build -t ub_cuda_sshd:$VERSION .
 
-#docker run -d -p 2222:2222 --name id_b_jd_v0 id_b:jd_v0
-
-# docker run  -dit -p 10200:22 --name $VERSION -h $VERSION ub_sshd:$VERSION
-# docker run  --runtime=nvidia -it -p 10200:22 --name $VERSION -h $VERSION ub_cuda_sshd:$VERSION
+# export VERSION=v2 && docker run  -dit -p 10204:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t -m 100m --cpuset-cpus="1,2" --name ${VERSION}_4 -h ${VERSION}_4 ub_sshd:$VERSION
 # ssh root@localhost -p 10200
 
 # lsi
@@ -13,7 +11,7 @@ docker build -t ub_cuda_sshd:$VERSION .
 
 # need change the ngrok, with its configure file
 # export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10203:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name $VERSION     -h $VERSION     ub_cuda_sshd:$VERSION
-# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10200:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_0 -h ${VERSION}_0 ub_cuda_sshd:$VERSION 
-# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10201:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_1 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
-# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10202:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --name ${VERSION}_2 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10200:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --cpuset-cpus="1,2" --name ${VERSION}_0 -h ${VERSION}_0 ub_cuda_sshd:$VERSION 
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10201:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --cpuset-cpus="3,4" --name ${VERSION}_1 -h ${VERSION}_1 ub_cuda_sshd:$VERSION
+# export VERSION=v21 && docker run  --runtime=nvidia -dit -p 10202:22 -v /mnt/jd/t/ana:/mnt/jd/t/ana -v /mnt/jd/t/vt:/root/jd/t --cpuset-cpus="5,6" --name ${VERSION}_2 -h ${VERSION}_2 ub_cuda_sshd:$VERSION
 
